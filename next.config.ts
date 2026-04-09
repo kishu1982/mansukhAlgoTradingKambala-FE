6e3fa11fea0da281f606c4203d1665d02c843c5a
kishu_chopra <158042875+kishu1982@users.noreply.github.com>
2026-04-09 10:25:17 +0530

    orign for cors added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   allowedDevOrigins: [
+    'http://188.208.141.112:3000',
+    'http://188.208.141.112:80',
+    'http://188.208.141.112:3001',
+    'http://188.208.141.112:5000'
+  ],
 };
 
 export default nextConfig;
